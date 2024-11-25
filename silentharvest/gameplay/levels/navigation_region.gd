26648529d69df95db326b2b80454f2786ec80cc5
Silver-Ink <martin.74@orange.fr>
2024-11-24 21:17:49 -0500

    wip navigation hole

--- silentharvest/gameplay/levels/navigation_region.gd
+++ silentharvest/gameplay/levels/navigation_region.gd
@@ -1,11 +1,12 @@
 extends NavigationRegion2D
 
 var new_navigation_polygon: NavigationPolygon = get_navigation_polygon()
+var geometry_data: NavigationMeshSourceGeometryData2D = NavigationMeshSourceGeometryData2D.new()
 
 func _ready():
 	pass
 	parse_2d_collisionshapes(self)
-	NavigationServer2D.bake_from_source_geometry_data(new_navigation_polygon, NavigationMeshSourceGeometryData2D.new());
+	NavigationServer2D.bake_from_source_geometry_data(new_navigation_polygon, geometry_data);
 	#new_navigation_polygon.make_polygons_from_outlines()
 	#set_navigation_polygon(new_navigation_polygon)
 
@@ -15,5 +16,6 @@ func parse_2d_collisionshapes(root_node: Node2D):
 			var area : CollisionPolygon2D = node.get_node("BlockingArea")
 			var collisionpolygon_transform: Transform2D = area.get_global_transform()
 			var collisionpolygon: PackedVector2Array = area.polygon
-			var cp = collisionpolygon_transform * collisionpolygon 
-			new_navigation_polygon.add_outline(cp)
+			var cp = collisionpolygon_transform * collisionpolygon
+			geometry_data.add_obstruction_outline(cp)
+			#new_navigation_polygon.add_outline(cp)
