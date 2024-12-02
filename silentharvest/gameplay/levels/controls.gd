a1071b3843993c7669ea14463e6cf682cfeaf979
Silver-Ink <martin.74@orange.fr>
2024-12-01 20:52:33 -0500

    fix menu with new font

--- silentharvest/gameplay/levels/controls.gd
+++ silentharvest/gameplay/levels/controls.gd
@@ -1,6 +1,7 @@
 class_name Controls extends BaseLevel
 
 @onready var btn_retour: Button = $HBoxContainer/VBoxContainer/Container/btn_retour
+@onready var sound_hover: AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready() -> void:
 	Inventory_Hud.hide()
@@ -8,3 +9,7 @@ func _ready() -> void:
 	
 func return_to_start() -> void:
 	get_node("/root/GameManager").load_level(3)
+
+
+func _on_btn_retour_mouse_entered() -> void:
+	sound_hover.play()
